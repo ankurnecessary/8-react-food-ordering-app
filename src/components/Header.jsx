055af7ba1e6b4c2bb7d686a39c36feb54edce9fd
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-10 04:35:09 +0930

    Added item count of the shopping cart in header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
+import CartContext from '../store/cartContext.jsx';
 
 export default function Header() {
-	return <header id='main-header'>
-		<div id='title'>
-			<img src={logoImg} alt='React food restaurant' />
-			<h1>ReactFood</h1>
-		</div>
-		<nav>
-			<Button textOnly>Cart (0)</Button>
-		</nav>
-	</header>;
+	const cartCtx = useContext(CartContext);
+	const totalCartItems = cartCtx.items.reduce((totalNoOfItems, item) => totalNoOfItems + item.quantity, 0);
+
+	return (
+		<header id='main-header'>
+			<div id='title'>
+				<img src={logoImg} alt='React food restaurant' />
+				<h1>ReactFood</h1>
+			</div>
+			<nav>
+				<Button textOnly>{`Cart (${totalCartItems})`}</Button>
+			</nav>
+		</header>
+	);
 }
