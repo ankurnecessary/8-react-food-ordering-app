8226198c7833e1a10fbe399dc296e3963d8d6eda
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-13 03:10:43 +0930

    Added UserProgressContext to keep track of whether user is on cart or checkout modal window.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import Meals from './components/Meals';
 import {CartContextProvider} from './store/CartContext.jsx';
+import {UserProgressContextProvider} from './store/UserProgressContext.jsx';
 
 function App() {
 	return (
+		<UserProgressContextProvider>
 		<CartContextProvider>
 			<Header />
 			<Meals />
 		</CartContextProvider>
+		</UserProgressContextProvider>
 	);
 }
 
