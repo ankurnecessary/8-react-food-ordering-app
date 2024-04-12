38ec12ac3bc335805ba6448e0fd947e8bc31130c
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-13 03:11:33 +0930

    Added cart modal window.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Header from './components/Header';
 import Meals from './components/Meals';
 import {CartContextProvider} from './store/CartContext.jsx';
 import {UserProgressContextProvider} from './store/UserProgressContext.jsx';
+import Cart from './components/Cart.jsx';
 
 function App() {
 	return (
 		<UserProgressContextProvider>
-		<CartContextProvider>
-			<Header />
-			<Meals />
-		</CartContextProvider>
+			<CartContextProvider>
+				<Header />
+				<Meals />
+				<Cart />
+			</CartContextProvider>
 		</UserProgressContextProvider>
 	);
 }
