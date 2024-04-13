284e0e13cf221b3dc01cdec6a46932c889c18d00
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-14 06:00:20 +0930

    Disabled indentation issues in eslint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,36 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'xo',
-		'plugin:react/recommended',
-	],
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [
-				'.eslintrc.{js,cjs}',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-		'linebreak-style': ['error', 'windows'], // To change the line break style preference from LF to CRLF
-		'no-unused-vars': 'warn', // To highlight declared but unused variables
-		'no-undef': 'error', // To highlight the usage of undeclared variables
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'xo',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'windows'], // To change the line break style preference from LF to CRLF
+    'no-unused-vars': 'warn', // To highlight declared but unused variables
+    'no-undef': 'error', // To highlight the usage of undeclared variables
+    indent: 'off',
+  },
 };
