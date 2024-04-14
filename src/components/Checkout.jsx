ab6e963524af8e18cfcb548bdd3142ea614aa6e8
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-15 07:42:18 +0930

    Preventing checkout form's default submission behavior and fetching the form data in form of object.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -17,12 +17,19 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd);
+    console.log(customerData);
+  }
+
   return (
     <Modal
       open={userProgressCtx.progress === 'checkout'}
       onClose={handleClose}
     >
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
