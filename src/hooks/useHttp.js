c5736a27a287843656a51e818ebe60645079a45b
Ankur Gupta <ankurnecessary@gmail.com>
2024-04-15 15:19:57 +0930

    Added a custom hook to handle the HTTP requests.

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,42 @@
+import {useEffect, useState, useCallback} from 'react';
+
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.message || 'Something went wrong. Please try again later');
+  }
+
+  return responseData;
+}
+
+export default function useHttp(url, config, initialData) {
+  const [data, setData] = useState(initialData);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendRequest = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const responseData = await sendHttpRequest(url, config);
+      setData(responseData);
+    } catch (error) {
+      setError(error.message || 'Something went wrong.');
+    }
+
+    setIsLoading(false);
+  }, [url, config]);
+
+  useEffect(() => {
+    if ((config && (config.method === 'GET' || !config.method)) || !config) {
+      sendRequest();
+    }
+  }, [sendRequest, config]);
+
+  return {
+    data,
+    error,
+    isLoading,
+    sendRequest,
+  };
+}
